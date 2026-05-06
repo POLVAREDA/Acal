a707bdb64a3babe768e16efcbe0e601ae15848b1
polvareda <p0lvared4@gmail.com>
2026-05-06 23:21:44 +0900

    46, commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const dogs = [
         age: 2,
         weight: 2.3,
         tags: ["활발해요", "사람좋아"],
-        image: "https://images.unsplash.com/photo-1591160674255-fc8b858cf5ad?q=80&w=300",
+        image: "",
     },
     {
         id: 2,
@@ -16,7 +16,7 @@ const dogs = [
         age: 3,
         weight: 3.5,
         tags: ["영리해요", "장난꾸러기"],
-        image: "https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?q=80&w=300",
+        image: "",
     },
     {
         id: 3,
@@ -25,7 +25,7 @@ const dogs = [
         age: 4,
         weight: 3.2,
         tags: ["조용해요", "애교많아요"],
-        image: "https://images.unsplash.com/photo-1516734212186-a967f81ad0d7?q=80&w=300",
+        image: "https://res.cloudinary.com/dkqqxjsek/image/upload/v1778076935/Gemini_Generated_Image_m591hlm591hlm591_gaadio.png",
     },
     {
         id: 4,
@@ -34,7 +34,7 @@ const dogs = [
         age: 5,
         weight: 4.8,
         tags: ["용감해요", "충성스러워요"],
-        image: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?q=80&w=300",
+        image: "",
     },
     {
         id: 5,
@@ -43,7 +43,7 @@ const dogs = [
         age: 6,
         weight: 5.1,
         tags: ["온순해요", "사람좋아"],
-        image: "https://images.unsplash.com/photo-1544568100-847a948585b9?q=80&w=300",
+        image: "",
     },
     {
         id: 6,
@@ -52,7 +52,7 @@ const dogs = [
         age: 4,
         weight: 4.0,
         tags: ["밝아요", "식탐있어요"],
-        image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?q=80&w=300",
+        image: "",
     },
     {
         id: 7,
@@ -61,7 +61,7 @@ const dogs = [
         age: 3,
         weight: 2.7,
         tags: ["소심해요", "따뜻해요"],
-        image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=300",
+        image: "",
     },
     {
         id: 8,
@@ -70,7 +70,7 @@ const dogs = [
         age: 5,
         weight: 6.0,
         tags: ["똑똑해요", "활발해요"],
-        image: "https://images.unsplash.com/photo-1587300003388-59208cc962cb?q=80&w=300",
+        image: "",
     },
     {
         id: 9,
@@ -79,7 +79,7 @@ const dogs = [
         age: 4,
         weight: 3.6,
         tags: ["애교많아요", "장난꾸러기"],
-        image: "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?q=80&w=300",
+        image: "",
     },
     {
         id: 10,
@@ -88,7 +88,7 @@ const dogs = [
         age: 7,
         weight: 5.4,
         tags: ["조용해요", "차분해요"],
-        image: "https://images.unsplash.com/photo-1510771463146-e89e6e86560e?q=80&w=300",
+        image: "",
     },
     {
         id: 11,
@@ -97,7 +97,7 @@ const dogs = [
         age: 3,
         weight: 3.0,
         tags: ["사람좋아", "애교많아요"],
-        image: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=300",
+        image: "",
     },
 ];
 
